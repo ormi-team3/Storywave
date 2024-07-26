2ed0e51a182c619f4f21a549c901f24845ae64a7
MoonSeokJun <tjrwns2513@naver.com>
2024-07-26 15:53:39 +0900

    lkl

---
+++ src/main/java/com/ormi/storywave/Category.java
@@ -0,0 +1,20 @@
+//package com.ormi.storywave;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import lombok.Getter;
+//import lombok.Setter;
+//import org.springframework.data.annotation.Id;
+//
+//@Entity
+//@Getter
+//@Setter
+//public class Category {
+//  @Id
+//  @GeneratedValue(strategy = GenerationType.IDENTITY)
+//  private Integer id;
+//
+//  private Integer postTypeId;
+//  private String categoryName;
+//}
